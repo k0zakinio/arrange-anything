bb6ef63b9cb6a9a244e477daa5b1657bec61c391
Andrew Dowell <k0zakinio@github.com>
2017-10-06 14:01:51 +0100

    channges

--- src/main/kotlin/com/company/k0zak/Routes.kt
+++ src/main/kotlin/com/company/k0zak/Routes.kt
@@ -3,8 +3,13 @@ package com.company.k0zak
 import org.http4k.core.HttpHandler
 import org.http4k.core.Response
 import org.http4k.core.Status
+import java.sql.Connection
+import java.sql.DriverManager
 
 object Routes {
+    private val url = "jdbc:testuser:testpassword//127.0.0.1:5432/testdb"
+    val con: Connection = DriverManager.getConnection(url)
+
     object GET {
         val event: HttpHandler = {
             Response(Status.OK).body("Nice one :)")
@@ -13,6 +18,13 @@ object Routes {
 
     object POST {
         val event: HttpHandler = {
+            val query = "INSERT TABLE foo (id SERIAL PRIMARY KEY, title TEXT);"
+            val prepareCall = con.prepareCall(query)
+
+            println("should be calling query: $query")
+
+            prepareCall.execute()
+
             Response(Status.CREATED).body("Something was meant to be created :)")
         }
     }
