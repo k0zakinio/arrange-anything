0e484ddf87a1603da0b78337c092570dc8036532
Andrew Dowell <k0zakinio@github.com>
2017-10-06 14:10:03 +0100

    some more changes

--- push-dockerhub.sh
+++ push-dockerhub.sh
@@ -2,5 +2,4 @@
 
 ./build.sh
 docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
-docker tag arrange-anything k0zakinio/arrange-anything
 docker push k0zakinio/arrange-anything
