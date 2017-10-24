78ed2df74d475030c66aa342eff4d9abae22bac2
k0zakinio <adowellphoto@gmail.com>
2017-10-24 11:29:37 +0100

    save database in project directory

--- start-postgres.sh
+++ start-postgres.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=testpassword -e POSTGRES_DB=testdb -v /var/lib/postgres/data:/var/lib/postgresql/data postgres:9.6
+docker run -d -p 5432:5432 \
+    -e POSTGRES_PASSWORD=testpassword \
+    -e POSTGRES_DB=testdb \
+    -v $(pwd)/database:/var/lib/postgresql/data \
+    postgres:9.6
