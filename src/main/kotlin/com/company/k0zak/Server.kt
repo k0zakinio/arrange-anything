8a6937075edcd2bdf3e6bbab8501b4d5eafb6b47
k0zakinio <adowellphoto@gmail.com>
2017-10-24 19:35:14 +0100

    Allows creation of new events through HTML

--- src/main/kotlin/com/company/k0zak/Server.kt
+++ src/main/kotlin/com/company/k0zak/Server.kt
@@ -3,9 +3,7 @@ package com.company.k0zak
 import com.company.k0zak.routes.NewEventsRoute
 import com.company.k0zak.routes.ViewEventsRoute
 import org.http4k.core.Method
-import org.http4k.routing.RoutingHttpHandler
-import org.http4k.routing.bind
-import org.http4k.routing.routes
+import org.http4k.routing.*
 import org.http4k.server.Jetty
 
 object Server {
@@ -15,9 +13,11 @@ object Server {
         val viewEvents = ViewEventsRoute(Dependencies.eventsDao)
 
         val app: RoutingHttpHandler = routes(
-                "/events" bind Method.POST to newEvents.post,
+//                "/new" bind Method.GET to newEvents.create,
+                "/new" bind Method.POST to newEvents.newPost,
                 "/view/{id}" bind Method.GET to viewEvents.byId,
-                "/view" bind Method.GET to viewEvents.all
+                "/view" bind Method.GET to viewEvents.all,
+                "/new" bind Method.GET to static(ResourceLoader.Classpath("public/new"))
         )
 
         val port = 8080
--- src/main/kotlin/com/company/k0zak/routes/NewEventsRoute.kt
+++ src/main/kotlin/com/company/k0zak/routes/NewEventsRoute.kt
@@ -4,13 +4,33 @@ import com.company.k0zak.dao.EventsDao
 import com.company.k0zak.model.Event
 import org.http4k.core.*
 import org.http4k.format.Jackson.auto
+import org.http4k.lens.*
 
 class NewEventsRoute(private val eventsDao: EventsDao) {
 
-    val post: HttpHandler = { req: Request ->
-        val eventLens = Body.auto<Event>().toLens()
-        val event = eventLens.extract(req)
-        eventsDao.insertEvent(event)
-        Response(Status.CREATED).body("Event: $event was inserted into the database")
+    data class JsonResponse(val message: String)
+    private val responseLens = Body.auto<JsonResponse>().toLens()
+
+    val newPost: HttpHandler = { req: Request ->
+        val ownerField = FormField.string().required("owner")
+        val titleField = FormField.string().required("title")
+
+        val formBody = Body.webForm(Validator.Strict, ownerField, titleField).toLens()
+
+        try {
+            val webform = formBody.extract(req)
+            val owner = ownerField.extract(webform)
+            val title = titleField.extract(webform)
+            eventsDao.insertEvent(Event(owner, title))
+
+            responseLens.inject(
+                    JsonResponse("A new event has been created with the title $title, owned by $owner"), Response(Status.CREATED))
+
+        } catch (e: LensFailure) {
+            println(e.message)
+            responseLens.inject(
+                    JsonResponse("Unable to create event because fields were missing!}"), Response(Status.BAD_REQUEST))
+        }
     }
+
 }
