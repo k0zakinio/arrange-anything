7c682a296be61517d9c027b0830411b33dedff26
k0zakinio <adowellphoto@gmail.com>
2017-10-26 15:01:52 +0100

    Don't need start-postgres now, using H2

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -e
 
 docker-compose down
 ./remove_arrange_anything.sh
-./start-postgres.sh
 ./gradlew clean build fatJar --stacktrace
 docker build -t k0zakinio/arrange-anything . 
--- start-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d -p 5432:5432 \
-    -e POSTGRES_PASSWORD=testpassword \
-    -e POSTGRES_DB=testdb \
-    -v /var/lib/aa_postgres_data:/var/lib/postgresql/data \
-    postgres:9.6
