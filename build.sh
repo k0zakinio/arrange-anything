ac107e508a0ad62825ce7c78e2a5c871ffa73b1c
k0zakinio <adowellphoto@gmail.com>
2017-10-26 14:44:49 +0100

    no need to stop postgres now as using H2 for testing

--- build.sh
+++ build.sh
@@ -2,18 +2,8 @@
 
 set -e
 
-function stop_postgres_if_running () {
-    RUNNING_POSTGRES_CONTAINER=$(docker ps | grep postgres | awk '{ print $1 }')
-    if [ ${RUNNING_POSTGRES_CONTAINER} ]
-        then
-            docker stop ${RUNNING_POSTGRES_CONTAINER}
-    fi
-}
-
-stop_postgres_if_running
 docker-compose down
 ./remove_arrange_anything.sh
 ./start-postgres.sh
 ./gradlew clean build fatJar --stacktrace
 docker build -t k0zakinio/arrange-anything . 
-stop_postgres_if_running
